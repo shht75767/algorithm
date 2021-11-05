eaca75e6c25399bd1ab115ae62f454f7aa27f80a
HyeonTae-No <88132674+shht75767@users.noreply.github.com>
2021-11-05 15:50:20 +0900

    for 문

--- 알고리즘-백준문제/for문/4.js
+++ 알고리즘-백준문제/for문/4.js
@@ -1,6 +1,17 @@
-const input = require('fs').readFileSync('./4.text').toString().split('\r\n');
+/*const input = require('fs').readFileSync('./4.text').toString().split('\r\n');
 
 for (let i = 1; i <= input[0]; i++) {
   let num = input[i].split(' ');
   console.log(Number(num[0]) + Number(num[1]));
+}*/
+
+let input = require('fs').readFileSync('./4.text').toString().split('\n');
+
+let answer = '';
+
+for (let i = 1; i <= input[0]; i++) {
+  let num = input[i].split(' ');
+  answer += Number(num[0]) + Number(num[1]) + '\n';
 }
+
+console.log(answer);
---
+++ 알고리즘-백준문제/for문/5.js
@@ -0,0 +1,5 @@
+let input = require('fs').readFileSync('./5.text').toString().split();
+
+for (let i = 1; i <= input[0]; i++) {
+  console.log(i);
+}
