11e58e62a77e7f5cffd78e297929a6850fa8ea74
HyeonTae-No <88132674+shht75767@users.noreply.github.com>
2021-11-08 23:55:29 +0900

    for 문

---
+++ 알고리즘-백준문제/for문/7.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('./7.text').toString().split('\r\n');
+
+for (let i = 1; i <= input[0]; i++) {
+  let numbers = input[i].split(' ');
+
+  console.log(`Case #${i}: ${Number(numbers[0]) + Number(numbers[1])}`);
+}
