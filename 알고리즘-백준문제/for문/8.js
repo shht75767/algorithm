fcaf0bab963a158773b0daf6c75ca4d2a79c24de
HyeonTae-No <88132674+shht75767@users.noreply.github.com>
2021-11-14 22:47:05 +0900

    for 문

--- 알고리즘-백준문제/for문/8.js
+++ 알고리즘-백준문제/for문/8.js
@@ -1,2 +1,9 @@
 const input = require('fs').readFileSync('./8.text').toString().split('\r\n');
-console.log(input);
+for (let i = 1; i <= input[0]; i++) {
+  let number = input[i].split(' ');
+  let a = Number(number[0]);
+  let b = Number(number[1]);
+  let c = a + b;
+
+  console.log(`Case #${i}: ${a} + ${b} = ${c}`);
+}
