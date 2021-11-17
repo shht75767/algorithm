545eaf1e2a1f851d3291f37a701d51b4ed84422b
HyeonTae-No <88132674+shht75767@users.noreply.github.com>
2021-11-17 22:24:04 +0900

    for문

--- 알고리즘-백준문제/for문/10.js
+++ 알고리즘-백준문제/for문/10.js
@@ -4,6 +4,23 @@ let num = Number(input);
 let result = '';
 
 for (let i = 0; i < num; i++) {
-        
+  result += '.';
+  console.log(result);
+}
+
+/* 
+   // 별
+    for (let line = 1; line <= N; ++line) {
+        const starCnt = line;
+        const spaceCnt = N - line;
+        let printString = '';
+        for (let i = 0; i < spaceCnt; ++i) {
+            printString += ' ';
+        }
+        for (let i = 0; i < starCnt; ++i) {
+            printString += '*';
+        }
+        console.log(printString);
     }
-  }
+}
+*/
