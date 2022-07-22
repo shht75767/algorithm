4f3c8476a0b6d2d7c89cb1053597e39e478cf2a8
HyeonTae-No <88132674+shht75767@users.noreply.github.com>
2022-07-22 17:11:45 +0900

    백준문제 배열 카운트

--- 알고리즘-백준문제/배열문/6.js
+++ 알고리즘-백준문제/배열문/6.js
@@ -1,13 +1,11 @@
 let input = require('fs').readFileSync('./6.text').toString().split('\r\n');
+let num = parseInt(input[0]);
 
-let number = Number(input[0]);
-
-for (let i = 1; i <= number; i++) {
+for (let i = 1; i <= num; i++) {
   let count = 0;
   let sum = 0;
-
   for (let j = 0; j < input[i].length; j++) {
-    if (input[i][j] === 'O') {
+    if (input[i][j] == 'O') {
       count++;
     } else {
       count = 0;
